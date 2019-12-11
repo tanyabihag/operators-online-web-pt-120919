c0a569f347021c3514916114c5b44b3a7cf31323
Tanya Bihag <tbihag@gmail.com>
2019-12-11 03:04:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,6 @@
 def unsafe?(speed)
  if speed < 40 || speed > 60
-   true
+  return true
   end
 
 end
@@ -8,7 +8,7 @@ end
 
 
 def not_safe?(speed)
-	  speed < 40 || speed > 60 ? "true" : "false"
+	  speed < 40 || speed > 60 ? true : false
 
 end
 	
